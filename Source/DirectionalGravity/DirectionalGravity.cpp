39570210141a09ee4700b3b5601a2a5dcdf7d08f
Tefel <lucas44@o2.pl>
2019-11-23 22:26:13 +0000

    4.23 update
    Updating to 4.23 version

--- Source/DirectionalGravity/DirectionalGravity.cpp
+++ Source/DirectionalGravity/DirectionalGravity.cpp
@@ -3,4 +3,4 @@
 #include "DirectionalGravity.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DirectionalGravity, "DirectionalGravity" );\
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DirectionalGravity, "DirectionalGravity" );
